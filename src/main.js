97a5c56a00d0055bd6ce4128a335ac588d02c7d9
johannagoenagac <johannagoenagac@gmail.com>
2019-01-17 15:57:02 -0300

    filtrando - ordenando - grafico con calculo - pasan test

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
- document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function(){
     window.M.AutoInit(); 
 });
 
 window.onload =() =>{
 
 
-showCards(POKEMON.pokemon)
+showCards(window.pokemon)
 
 function showCards(filterData) {
 
@@ -15,72 +15,150 @@ document.getElementById('root').innerHTML = '';
     for (let i = 0; i < filterData.length; i++) {
         document.getElementById('pokemon-list').innerHTML += `
             <div class="col s6 m3" >
-            <div class="card">
-                <img class="imagen-lol responsive-img" src="${filterData[i].img}" >
-                <div class="card-content">
-                    <span class="card-title activator grey-text text-darken-2"><h6>${filterData[i].name}</h6><i class="material-icons right">more_vert</i></span>
+                <div class="card">
+                    <img class="imagen-lol responsive-img" src="${filterData[i].img}" >
+                    <div class="card-content">
+                        <span class="card-title activator grey-text text-darken-2"><h6>${filterData[i].name}</h6><i class="material-icons right">more_vert</i></span>
                     </div>
                     <div class="card-reveal">
-                    <span class="card-title grey-text text-darken-4">${filterData[i].name}<i class="material-icons right">close</i></span>
-                    <p> Numero: ${ filterData[i].num }</p>
-                    <p> Tipo: ${ filterData[i].type}</p>
-                    <span class="link" data-pokemon='${filterData[i].id}'></span>
+                        <span class="card-title grey-text text-darken-4">${filterData[i].name}<i class="material-icons right">close</i></span>
+                        <p> Numero: ${ filterData[i].num }</p>
+                        <p> Tipo: ${ filterData[i].type}</p>
+                        <span class="link" data-pokemon='${filterData[i].id}'></span>
                     </div>
                 </div>
-                </div> ` 
+            </div> ` 
       }
 }
-
+    //Evento para mostrar data filtrada en el select filtrar
     document.getElementById('select-type').addEventListener('change',()=>{
         let condition = document.getElementById('select-type').value;
         // console.log(condition);
-        let filterData = window.data.filterData(POKEMON.pokemon, condition);
+        let filterData = window.data.filterData(window.pokemon, condition);
         // console.log(filterData);
         showCards(filterData);   
       });
 
 
 
-      showOrder(POKEMON.pokemon)
+    //   showOrder(POKEMON.pokemon)
 
-    function showOrder(sortData) {
+    // function showOrder(sortData) {
       
-      document.getElementById('root').innerHTML = '';
+    //   document.getElementById('root').innerHTML = '';
       
-        document.getElementById('pokemon-list').innerHTML = '';
-          for (let i = 0; i < POKEMON.pokemon.length; i++) {
-              document.getElementById('pokemon-list').innerHTML += `
-                  <div class="col s6 m3" >
-                  <div class="card">
-                      <img class="imagen-pokemon responsive-img" src="${POKEMON.pokemon[i].img}" >
-                      <div class="card-content">
-                          <span class="card-title activator grey-text text-darken-2"><h6>${POKEMON.pokemon[i].name}</h6><i class="material-icons right">more_vert</i></span>
-                          </div>
-                          <div class="card-reveal">
-                          <span class="card-title grey-text text-darken-4">${POKEMON.pokemon[i].name}<i class="material-icons right">close</i></span>
-                          <p> Numero: ${POKEMON.pokemon[i].num }</p>
-                          <p> Tipo: ${POKEMON.pokemon[i].type}</p>
-                          <span class="link" data-pokemon='${POKEMON.pokemon[i].id}'></span>
-                          </div>
-                      </div>
-                      </div> ` 
-            }
-      }
+    //     document.getElementById('pokemon-list').innerHTML = '';
+    //       for (let i = 0; i < POKEMON.pokemon.length; i++) {
+    //           document.getElementById('pokemon-list').innerHTML += `
+    //               <div class="col s6 m3" >
+    //               <div class="card">
+    //                   <img class="imagen-pokemon responsive-img" src="${POKEMON.pokemon[i].img}" >
+    //                   <div class="card-content">
+    //                       <span class="card-title activator grey-text text-darken-2"><h6>${POKEMON.pokemon[i].name}</h6><i class="material-icons right">more_vert</i></span>
+    //                       </div>
+    //                       <div class="card-reveal">
+    //                       <span class="card-title grey-text text-darken-4">${POKEMON.pokemon[i].name}<i class="material-icons right">close</i></span>
+    //                       <p> Numero: ${POKEMON.pokemon[i].num }</p>
+    //                       <p> Tipo: ${POKEMON.pokemon[i].type}</p>
+    //                       <span class="link" data-pokemon='${POKEMON.pokemon[i].id}'></span>
+    //                       </div>
+    //                   </div>
+    //                   </div> ` 
+    //         }
+    //   }
 
-    document.getElementById('select-order').addEventListener('change',() => {
+
+    //Evento imprimir nombre ordenado de a-z y z-a en select ordenar
+    document.getElementById('select-order').addEventListener('change',() => {//metodo change me permite elegir una opcion diferente cada vez
         let sortOrder = document.getElementById('select-order').value;
-        let dataSort = window.data.sortData(POKEMON.pokemon,'name',sortOrder);
-        console.log(sortOrder)
+        let dataSort = window.data.sortData(window.pokemon,'name',sortOrder);
         showCards(dataSort);
     });
 
+    //Evento imprimir nombre ordenado de 1-151 y 151-1 en select ordenar
+    document.getElementById('select-order').addEventListener('change',() => {
+        let sortOrder = document.getElementById('select-order').value;
+        let dataSortNumber = window.data.sortData(window.pokemon,'num',sortOrder);
+        showCards(dataSortNumber);
+    });
 
+    document.getElementById("start").addEventListener("click",
+        (event) => {
+        event.preventDefault();
+        document.getElementById("page1").style.display="block";
+        document.getElementById("page2").style.display="none";
+    });
+
+
+    //Muestra grafico al presionar estadisticas
+    document.getElementById("stats").addEventListener("click",
+        (event) => {
+        event.preventDefault();
+        document.getElementById("page1").style.display="none";
+        document.getElementById("page2").style.display="block";
+    });
+
+
+
+    //Grafico estadisticas, cantidad de pokemones por tipo
+    window.google.charts.load("current",{packages:["corechart"]});
+    window.google.charts.setOnLoadCallback(drawChart);
+      
+    function drawChart() {
+        var data = window.google.visualization.arrayToDataTable([
+        ['Tipo Pokemon', 'Cantidad por tipo'],
+        ['Grass',      window.data.filterData(window.window.pokemon,"Grass").length],
+        ['Poison',     window.data.filterData(window.window.pokemon,'Poison').length],
+        ['Flying',     window.data.filterData(window.window.pokemon,'Flying').length],
+        ['Fire',       window.data.filterData(window.window.pokemon,'Fire').length],
+        ['Water',      window.data.filterData(window.window.pokemon,'Water').length],
+        ['Bug',        window.data.filterData(window.window.pokemon,'Bug').length],
+        ['Normal',     window.data.filterData(window.window.pokemon,'Normal').length],
+        ['Electric',   window.data.filterData(window.window.pokemon,'Electric').length],
+        ['Ground',     window.data.filterData(window.window.pokemon,'Ground').length],
+        ['Fighting',   window.data.filterData(window.window.pokemon,'Fighting').length],
+        ['Psychic',    window.data.filterData(window.window.pokemon,'Psychic').length],
+        ['Rock',       window.data.filterData(window.window.pokemon,'Rock').length],
+        ['Ice',        window.data.filterData(window.window.pokemon,'Ice').length],
+        ['Ghost',      window.data.filterData(window.window.pokemon,'Ghost').length],
+        ['Dragon',     window.data.filterData(window.window.pokemon,'Dragon').length],
+       
+        ]);
+
+        var options = {
+            'width':900, 
+            'heigth':500,
+            title: 'My Daily Activities',
+            is3D: true,
+        };
+        
+        var chart = new window.google.visualization.PieChart(document.getElementById('piechart_3d'));
+        chart.draw(data, options);
+        }
+
+
+    //Menu responsive
+    
     document.addEventListener('DOMContentLoaded', function() {
         var elems = document.querySelectorAll('.sidenav');
-        var instances = M.Sidenav.init(elems);
-      });//Nav Responsive
+        window.M.Sidenav.init(elems);
+    
+      });
 
+      document.getElementById("start-1").addEventListener("click",
+      (event) => {
+      event.preventDefault();
+      document.getElementById("page1").style.display="block";
+      document.getElementById("page2").style.display="none";
+  });
 
+    //Imprimir grafico en opcion estadisticas del menu responsive
+    document.getElementById("stats-1").addEventListener("click",
+        (event) => {
+        event.preventDefault();
+        document.getElementById("page1").style.display="none";
+        document.getElementById("page2").style.display="block";
+    });
     }
 
     
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,12 +1,53 @@
-require('../src/data.js');
+global.window=global;//Llama al test contenido aquí
+global.assert=require('chai').assert;//
+require('../src/data.js');//require: lo que requiero para que corra mi test
+require('../src/data/pokemon/pokemon.js');
+
+describe('pokemon', () => {
+    it('Debería ser un objeto', () => {
+        window.assert.equal(typeof window.pokemon, 'object');//funcion (parametros a comparar) typeof= me dice si es un objeto
+      });
+
+    describe('data.filterData',()=>{ //testenado funcion filter
+        it('Debería ser una funcion',()=>{
+            window.assert.equal(typeof window.data.filterData,'function');//typeof me muestra lo que 
+        });
+
+        it('Debería retornar el primer pokemon del tipo planta',()=>{
+            let pokemonGrass=window.data.filterData(window.pokemon,'Grass')[0].name;
+            window.assert.equal(pokemonGrass,'Bulbasaur');
+        });
+
+        it('Debería retornar el tercer pokemon del tipo agua ',()=>{
+            let pokemonWater=window.data.filterData(window.pokemon,'Water')[2].name;
+            window.assert.equal(pokemonWater,'Blastoise');   
+        });        
+
+        });
+    
+      })
+
+      describe('data.sortData',()=>{ //testenado funcion sort
+        it('Debería ser una funcion',()=>{
+            window.assert.equal(typeof window.data.sortData,'function');//typeof si es una función
+        });
+
+        it('Deberia retornar el primer pokemon con letra a',()=>{
+          let pokemonLetterA=window.data.sortData(window.pokemon,'name','a-z')[0].name;
+          window.assert.equal(pokemonLetterA,'Abra');
+      });
+
+      it('Debería retornar el numero 50 de los pokemones ',()=>{
+          let pokemonNumber50=window.data.sortData(window.pokemon,'num','1-151')[49].name;
+          window.assert.equal(pokemonNumber50,'Diglett');   
+      });  
+      
+      it('Debería retornar el numero 120 de los pokemones ',()=>{
+        let pokemonNumber120=window.data.sortData(window.pokemon,'num','151-1')[31].name;
+        window.assert.equal(pokemonNumber120,'Staryu');   
+    });        
 
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
-  });
-
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
-});
+      });
+  
+    
